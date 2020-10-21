c4e1ff74fef807b388324a0cf469592ce078e386
Nirajara <32470269+Nirajara@users.noreply.github.com>
2020-10-21 12:47:34 -0700

    Basic Servo Motor Code

---
+++ Basic_Servo_Motor_Code.ino
@@ -0,0 +1,19 @@
+#include <Servo.h> //importing servo library
+
+Servo servo;
+int servoPin = 9; 
+int servoPosition = 90; //0 to 180
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  servo.attach(servoPin);
+  servo.write(servoPosition); 
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
